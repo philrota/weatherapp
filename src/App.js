ea73f129b4aa0b17c34eda1f4984af51b3ab812c
philrota <58667114+philrota@users.noreply.github.com>
2020-03-15 12:09:47 +0100

    Features
    -new dinamic background changes within temperature
    -location
    -next hours and next days weather summary

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+
 function App() {
   let date = new Date();
 
@@ -29,47 +30,65 @@ function App() {
   let day = days[date.getDay()];
   let month = months[date.getMonth()];
 
-  window.addEventListener("load",()=>{
-    let lat,long
-    let tempdesc = document.querySelector('.temp')
-    let summ = document.querySelector('.weather')
-    let zone = document.querySelector('.location')
-    let button = document.querySelector('.conv-btn')
-    let buttonToF= document.querySelector(".convback-btn")
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(position=>{
-        long=position.coords.longitude;
-        lat=position.coords.latitude;
-        const proxy = 'https://cors-anywhere.herokuapp.com/'
+  window.addEventListener("load", () => {
+    let lat, long;
+    let tempdesc = document.querySelector(".temp");
+    let summ = document.querySelector(".weather");
+    let nextDays = document.querySelector(".next-days")
+    let zone = document.querySelector(".location");
+    let button = document.querySelector(".conv-btn");
+    let buttonToF = document.querySelector(".convback-btn");
+    let body = document.querySelector(".App");
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        const proxy = "https://cors-anywhere.herokuapp.com/";
         const api = `${proxy}https://api.darksky.net/forecast/2042d57909f681f7fc84a5b6d0166b9f/${lat},${long}`;
-        
+
         fetch(api)
-      .then(res=>{return res.json()})
-      .then(data => {console.log(data);
-      const{temperature} =data.currently
-      
-      //manipolo il dom
-      tempdesc.textContent = temperature + " °F"
-      summ.textContent = data.daily.summary
-      zone.textContent = data.timezone
-      button.addEventListener("click",()=>{
-        tempdesc.textContent = Math.round((temperature-32)*5/9) + " °C"
-      })
-      buttonToF.addEventListener("click",()=>{
-        tempdesc.textContent = temperature + " °F"
-      })
-  
-    
-    });
-      
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            console.log(data);
+            const { temperature } = data.currently;
+
+            //manipolo il dom
+            tempdesc.textContent = temperature + " °F";
+            summ.textContent = data.hourly.summary;
+            nextDays.textContent=data.daily.summary
+
+            button.addEventListener("click", () => {
+              tempdesc.textContent =
+                Math.round(((temperature - 32) * 5) / 9) + " °C";
+            });
+            buttonToF.addEventListener("click", () => {
+              tempdesc.textContent = temperature + " °F";
+            });
+
+            if (temperature > 77) {
+              body.setAttribute("class", "App-hot");
+            } else if (temperature > 59) {
+              body.setAttribute("class", "App-warm");
+            } else if (temperature > 46.4) {
+              body.setAttribute("class", "App-coldy");
+            }
+          });
       });
-      
     }
+    const ipStack = "http://www.geoplugin.net/json.gp";
+    fetch(ipStack)
+      .then(response => {
+        return response.json();
+      })
+      .then(datas => {
+        console.log(datas);
+        zone.textContent = datas.geoplugin_regionName;
+      });
   });
-    
-   
-  
-  
+
   return (
     <div className="App">
       <main>
@@ -89,10 +108,17 @@ function App() {
         <div className="weather-box">
           <div className="temp"> </div>
           <div className="converter">
-            <button className ="conv-btn"type="button">Convert to Celsius</button>
-            <button className="convback-btn" type="button">Convert to Fahrenheit</button>
-            </div>
+            <button className="conv-btn" type="button">
+              Convert to Celsius
+            </button>
+            <button className="convback-btn" type="button">
+              Convert to Fahrenheit
+            </button>
+          </div>
+          
+
           <div className="weather"></div>
+          <div className="next-days"></div>
         </div>
       </main>
     </div>
